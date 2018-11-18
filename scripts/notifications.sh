28c2e6947a5fc39267e9d8bb48dfbc32693758c3
leachim <leachim@users.noreply.github.com>
2018-11-18 14:27:02 +0000

    minor fix for notification daemon, support multiple servers and clients

--- scripts/notifications.sh
+++ scripts/notifications.sh
@@ -2,6 +2,6 @@
 
 while true; 
 do 
-  nc -l -p 58439 | while read OUTPUT; do notify-send "$(echo ${OUTPUT} | cut -f1 -d ";")" "\n$(echo ${OUTPUT} | cut -f2 -d ";")" -u low ; done; sleep 1; 
+  nc -l -p 58439 -p 58440 -p 58441 | while read OUTPUT; do notify-send "$(echo ${OUTPUT} | cut -f1 -d ";")" "\n$(echo ${OUTPUT} | cut -f2 -d ";")" -u low ; done; sleep 1;
 done
 
