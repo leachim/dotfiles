8cda2a06bf4868596979242405bb635fb5481f29
leachim <leachim@users.noreply.github.com>
2020-04-11 17:02:39 +0100

    update backup processes

--- desktop/.backup/zotero-backup.sh
+++ desktop/.backup/zotero-backup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Backup zotero folder to tar archive
-TIME=`date +%d_%m_%y`            # This Command will add date in Backup File Name.
-FILENAME=zotero-archive-$(hostname)-$TIME.tar.gz    # Here i define Backup file name format.
+TIME=`date +%Y_%m_%d`            # This Command will add date in Backup File Name.
+FILENAME=zotero-$(hostname)-$TIME.archive.tar.gz    # Here i define Backup file name format.
 DESDIR=~/.backup/         # Destination of backup file.
 tar cpzf $DESDIR/$FILENAME -C $HOME/.zotero/ zotero
 #END
